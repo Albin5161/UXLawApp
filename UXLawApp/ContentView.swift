0b2109c1a23a9d2de85fbc5a374eacd8749afb07
Albin5161 <5161.albin@gmail.com>
2026-05-07 16:56:32 +0530

    Initial Commit

---
+++ UXLawApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UXLawApp
+//
+//  Created by Albin Sigi on 7/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UXLawApp/UXLawAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UXLawAppApp.swift
+//  UXLawApp
+//
+//  Created by Albin Sigi on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct UXLawAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
